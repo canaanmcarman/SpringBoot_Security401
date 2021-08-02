706b1696ee2dbecf31167bcd66fa55394d1c4ac1
canaanmcarman <canaan.m.carman@gmail.com>
2021-08-02 13:15:38 -0400

    first

---
+++ src/main/java/com/example/springboot_security/HomeController.java
@@ -0,0 +1,16 @@
+package com.example.springboot_security;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+    @RequestMapping("/2")
+    public String page2() {
+        return "page2";
+    }
+}
